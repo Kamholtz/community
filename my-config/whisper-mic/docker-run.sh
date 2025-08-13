1e89c21cefb88e137189a3b6648a16543e347ffd
Carl <carl.kamholtz@gmail.com>
2025-08-13 17:09:23 +1000

    feat: Update Docker configuration for whisper-mic with improved model size and audio settings for enhanced performance

--- my-config/whisper-mic/docker-run.sh
+++ my-config/whisper-mic/docker-run.sh
@@ -9,11 +9,13 @@ echo "Starting whisper-mic in Docker container..."
 export USER_ID=$(id -u)
 export GROUP_ID=$(id -g)
 
-# Set default environment variables
-export MODEL_SIZE=${MODEL_SIZE:-tiny}
+# Set default environment variables  
+export MODEL_SIZE=${MODEL_SIZE:-small}
 export DEVICE=${DEVICE:-auto}
 export ENGLISH_ONLY=${ENGLISH_ONLY:-true}
 export USE_FASTER=${USE_FASTER:-true}
+export ENERGY_THRESHOLD=${ENERGY_THRESHOLD:-300}
+export PAUSE_THRESHOLD=${PAUSE_THRESHOLD:-0.8}
 
 # Build and run the container
 docker compose up --build whisper-mic
