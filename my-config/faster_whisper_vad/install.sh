842aeca092705145947827c500ee21eb942ec692
Carl <carl.kamholtz@gmail.com>
2025-08-17 14:25:04 +1000

    feat: update *.talon-list

--- my-config/faster_whisper_vad/install.sh
+++
@@ -1,4 +0,0 @@
-pip install sounddevice webrtcvad faster-whisper pynput numpy
-# Linux X11: sudo apt install xdotool
-# Linux Wayland (wlroots): sudo apt install wtype
-# Windows: pip install keyboard
--- my-config/whisper/run_faster_whisper.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-"venv_faster_whisper/bin/python" "dictate_faster_whisper.py" "$@"
