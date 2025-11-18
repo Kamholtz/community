6c2b3cb57e15d735546a2ac524549c345fabfe07
Carl <carl.kamholtz@gmail.com>
2025-11-18 18:12:40 +1000

    feat: update type hints for edit_command function to use Union for better clarity

--- core/edit/edit_command.py
+++ core/edit/edit_command.py
@@ -1,3 +1,5 @@
+from typing import Union
+
 from talon import Module, actions, settings
 
 from .edit_command_actions import EditAction, EditSimpleAction, run_action_callback
@@ -165,7 +167,7 @@ compound_actions = {
 
 @mod.action_class
 class Actions:
-    def edit_command(action: EditAction | str, modifier: EditModifier | str):
+    def edit_command(action: Union[EditAction, str], modifier: Union[EditModifier, str]):
         """Perform edit command with associated modifier.
         Action and modifier can be dataclasses (formed from utterances via
         capture) or str, for use in scripts. Strings should match the action or
